d0893d49197eadb52d3c858bb3fbb6f9a15095a2
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-26 08:55:25 -0500

    two way binding

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,12 @@
 var app = angular.module('FoodApp', []);
 
+
 app.controller('FoodController', function(){
   console.log('FoodController loaded');
-});
+  var self = this; // holds the value of this
+  self.testModel = 'this is bound two ways, yo!';
+  }
+);
 
-var self = this; // holds the value of this
+
+
